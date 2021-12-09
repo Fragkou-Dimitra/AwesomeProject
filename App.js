6df31b5d48f78a0ceec0883e4e4a8717562f5233
Fragkou-Dimitra <74416946+Fragkou-Dimitra@users.noreply.github.com>
2021-12-09 17:38:08 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 import 'react-native-gesture-handler';
-import React,  {  useState } from 'react';
+import React,  { useEffect, useState } from 'react';
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
@@ -24,6 +24,8 @@ import {
   Image,
   Button,
   FlatList,
+  TouchableOpacity,
+  Linking,
   TouchableRipple,
   Switch
 } from 'react-native';
@@ -35,13 +37,14 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
+const SectionNews = ({children, title, subtitle}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
       <Text style={[styles.sectionTitle, {color: "red",},]}>
         {title}
       </Text>
+      <Text >{subtitle}</Text>
       <Text style={[ styles.sectionDescription, {color: isDarkMode ? Colors.light : Colors.dark, },]}>
         {children}
       </Text>
@@ -64,31 +67,67 @@ const Home: () => Node = ({navigation}) => {
          <View style={styles.MyHeader}>
             <Image style={{ height:200, width:200}} source={require('./images/programming_icon.png') }/>
             <QueryClientProvider client={queryClient}>
-        <User />
-      </QueryClientProvider>
+            <User />
+           </QueryClientProvider>
           </View> 
-        <View style={{backgroundColor: isDarkMode ? Colors.black : Colors.white }}>           
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-          <Button onPress={() => navigation.navigate('Tasks')} title="My Tasks"/>
+        <View style={{backgroundColor: isDarkMode ? Colors.black : Colors.white }}> 
+          <Text style={{alignContent:'center',fontWeight:"300", fontSize:30, marginTop:30, padding:10}}>News</Text>
+          <View style={{borderBottomColor: 'black',borderBottomWidth: 1}}/>       
+          <SectionNews title="Why broken African phones are shipped to Europe" subtitle="Eric Arthur does not have much time for hobbies - he spends most weekends driving all over Ghana collecting broken mobile phones.">
+           <Text> 
+             <View style={{width:300, flex:1, flexDirection:'row'}}>
+               <Image style={{ height:100, width:100,marginRight:10}} source={{uri:'https://ichef.bbci.co.uk/news/976/cpsprodpb/109E3/production/_121476086_whatsappimage2021-11-08at10.55.43.jpg'}}/>
+               <Text style={{ flexShrink: 1 }}> From his home in Cape Coast he can rove more than 100 miles (160km) in one weekend visiting repair shops and scraps yards - anywhere that has a decent supply of broken devices. In a good weekend he can collect 400 of them. On top of that, he manages a team of six agents doing the same thing in other parts of the country, and between them they expect to collect around 30,000 phones this year.
+                  <Text style={{color: 'blue'}}
+                    onPress={() => Linking.openURL('https://www.bbc.com/news/business-59161682')}>
+                    continue reading...
+                  </Text>
+               </Text>  
+             </View>
+           </Text> 
+          </SectionNews>
+          <SectionNews title="Myanmar coup: The women abused and tortured in detention" subtitle="Women in Myanmar have been tortured, sexually harassed and threatened with rape in custody, according to accounts obtained by the BBC.">
+           <Text> 
+             <View style={{width:300, flex:1, flexDirection:'row'}}>
+               <Image style={{ height:100, width:100,marginRight:10}} source={{uri:'https://ichef.bbci.co.uk/news/976/cpsprodpb/9908/production/_121867193_image005.jpg'}}/>
+               <Text style={{ flexShrink: 1 }}> Five women who were detained for protesting against a military coup in the country earlier this year say they were abused and tortured in the detention system after their arrests.
+                  Their names have been changed in the following accounts to protect their safety.<Text style={{fontWeight:"bold"}}> Warning: this piece contains disturbing descriptions of abuse. </Text>Since Myanmar's military seized power in February, protests have swept across the country - and women have played a prominent role in the resistance movement. 
+                  <Text style={{color: 'blue'}}
+                    onPress={() => Linking.openURL('https://www.bbc.com/news/world-asia-59462503')}>
+                    continue reading...
+                  </Text>
+               </Text>  
+             </View>
+           </Text> 
+          </SectionNews>
+          <Footer/>
         </View>
       </ScrollView>
     </SafeAreaView>
+    
   );
 };
 
+const Footer:()=>Node=()=>{
+  return(
+    <ScrollView style = {{backgroundColor:'black', marginTop:40, padding:20}}>
+          <View style={{flex:1, flexDirection:"row", justifyContent:"space-around"}}>
+            <View>
+                  <TouchableOpacity onPress={() => Linking.openURL('https://www.facebook.com')}> 
+                  <Icon name="facebook" size={40} color='#3B5998'/>
+                </TouchableOpacity ></View>
+                <View>
+                  <TouchableOpacity onPress={() => Linking.openURL('https://www.twitter.com')}> 
+                  <Icon name="twitter" size={40} color='#00ACEE'/>
+                </TouchableOpacity ></View>
+                <View>
+                  <TouchableOpacity onPress={() => Linking.openURL('https://www.linkedin.com')}> 
+                  <Icon name="linkedin" size={40} color='#0E76A8'/>
+                </TouchableOpacity ></View>
+          </View>
+      </ScrollView>
+  )
+}
 
 const User:()=>Node=()=> {
   const { isLoading, error, data } = useQuery('name', () =>
@@ -99,7 +138,6 @@ const User:()=>Node=()=> {
   if (isLoading) return <Text>'Loading...'</Text> 
   if (error) return <Text>'An error has occurred: '  {error.message}</Text>
   return (
-    
     <View>    
       <Text h1>{data.name}{' '}</Text>
       <Text style={{color:'blue'}}>{data.email}{' '}</Text>
@@ -110,13 +148,14 @@ const User:()=>Node=()=> {
 const Tasks:()=>Node=({navigation})=> {
   const queryClient = new QueryClient();
   return (
+    <ScrollView>
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={[styles.sectionContainer, styles.sectionTitle]}>Task-list</Text>
       <QueryClientProvider client={queryClient}>
         <ToDoList />
       </QueryClientProvider>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
     </View>
+  <Footer/></ScrollView>
   );
 }
 
@@ -125,6 +164,7 @@ const Tasks:()=>Node=({navigation})=> {
 const ToDoList:()=>Node=()=> {
   const [data,setData] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const [statusIndicator1,setStatus]=useState('not done');
   useQuery('id' , async () => {
     try {
      const response = await fetch('https://my-json-server.typicode.com/Fragkou-Dimitra/demo/db');
@@ -139,7 +179,8 @@ const ToDoList:()=>Node=()=> {
    }
  }
   );
- 
+ // useEffect(() => { statusIndicator1 = 'done';  });
+
   return (
     <View style={styles.listContainer}>
         {isLoading ? <ActivityIndicator/> : (
@@ -149,39 +190,47 @@ const ToDoList:()=>Node=()=> {
             renderItem={({ item }) => (
               <View style={styles.row}>
                 <View style={styles.row_cell_title}><Text style={styles.row_title}>{item.title}{' : '}</Text></View>          
-                <Text style={styles.row_cell_status}>{item.status=='done'?
-                (<Text style={{color:'green'}}>done<Icon name="check"/></Text>):
-                (<Text style={{color:'red'}}>not done<Icon name="star"/>
-                   <Button onPress={()=>{item.status='done'}} title='done'/>
-                </Text>)}
-                </Text> 
+                <View style={styles.row_cell_status}>{item.status=='done'?
+                ( 
+                  <View><Icon name="check" size={40} color='black' />
+                
+                </View>):
+                (<View >  
+                  <View><Icon name="star" size={40} color='red'/>
+                  <TouchableOpacity onPress={() => setStatus(statusIndicator1) }> 
+                    <Text style = {styles.doneButton}>done</Text>
+                </TouchableOpacity ></View>
+                </View>)}
+                </View> 
               </View>
             )}
           />
         )}
       </View>
   )
-}
+  }
 
 const DrawerContent=(props):Node=> {
   
   return(
       <View style={{flex:1, backgroundColor:'black'}}>
-          <DrawerContentScrollView {...props}>
+          <DrawerContentScrollView {...props} >
               <View style={styles.drawerContent}>                
-                      <DrawerItem 
-                          icon={({color, size}) => ( <Icon name="home-outline" color={color} size={size} />)}
+                      <DrawerItem  //{...props} activeTintColor='blue' activeBackgroundColor='rgba(250, 250, 250, .04)'
+                          icon={({color, size}) => (<Icon name="home-outline" color={color} size={size}/>)}
                           label="Home"
                           onPress={() => {props.navigation.navigate('Home')}}
+                          contentOptions= {{
+                            activeTintColor: 'red',
+                            activeBackgroundColor:'rgba(250, 250, 250, .04)',
+                            itemsContainerStyle: {
+                              marginVertical: 0,
+                            }}
+                            
+                          }
                       />
                       <DrawerItem 
-                          icon={({color, size}) => (
-                              <Icon 
-                              name="calendar" 
-                              color={color}
-                              size={size}
-                              />
-                          )}
+                          icon={({color, size}) => (<Icon name="calendar" color={color} size={size}/>)}
                           label="My Tasks"
                           onPress={() => {props.navigation.navigate('Tasks')}}
                       />                
@@ -231,7 +280,8 @@ const styles = StyleSheet.create({
   },
   drawerContent: {
     flex: 1,
-    backgroundColor:'white'
+    backgroundColor:'white',
+   
 
   },
    
@@ -242,7 +292,7 @@ const styles = StyleSheet.create({
     justifyContent: "center", 
     alignItems: "center", 
     backgroundColor: 'black',
-    width:300
+    width:400
   },
   flatListContainer: {
     marginTop: 14,
@@ -269,17 +319,25 @@ const styles = StyleSheet.create({
     textAlignVertical: "top",
     includeFontPadding: false,
     flex: 0,
-    fontSize: 20,
+    fontSize: 20
   },
   row_cell_status: {
     color: 'white',
-    paddingLeft: 16,
+    padding: 16,
     flex: 0,
-    fontSize: 15
+    fontSize: 15,
+    
   },
   row_cell_title: {
     flex: 1
   },
+  doneButton:{
+    textAlign:'center',
+    borderRadius: 3,
+    backgroundColor: 'black',
+    color: 'white',
+    fontSize:10
+  }
   
 });
 
